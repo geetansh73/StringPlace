12dc345862410c5f5a09101dc546e3f2b5ec5203
Geetansh Gupta <67698473+geetansh73@users.noreply.github.com>
2021-02-24 14:15:36 +0530

    Add files via upload

---
+++ stringplace.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class axax
+{
+public static void main(String g[])
+{
+    Scanner sc=new Scanner(System.in);
+String s="axax";
+System.out.println(s);
+String b=sc.nextLine();
+String a=sc.next();
+
+
+
+System.out.println(a);
+System.out.println(b);
+}}
